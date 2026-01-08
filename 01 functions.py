6f66f1a850406d420eba8ede774f8237bef6eb75
its-mohan79 <mohanroy8989@gmail.com>
2026-01-08 22:24:01 +0530

    complete the lesson 1

---
+++ 01 functions.py
@@ -0,0 +1,36 @@
+# a=int(input("enter your number: "))
+# b=int(input("enter your number: "))
+# c=int(input("enter your number: "))
+
+# average=(a+b+c)/3
+# print(average)
+
+# a=int(input("enter your number: "))
+# b=int(input("enter your number: "))
+# c=int(input("enter your number: "))
+
+# average=(a+b+c)/3
+# print(average)
+
+
+# using functions for easiast way
+
+
+def avg():
+    a=int(input("enter your number: "))
+    b=int(input("enter your number: "))
+    c=int(input("enter your number: "))
+
+    average=(a+b+c)/3
+    print(average)
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
+print("Thank you")
+avg()
